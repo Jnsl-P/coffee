ccac91d29430d915b15836044706ad9d71effd1e
Jansel2 <pingueljansel@gmail.com>
2025-05-22 08:17:19 +0800

    final

--- image.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-class ImageLink():
-    def __init__(self):
-        self.image_path = r"c:\Users\user\OneDrive\Desktop\Cvsu beans test\20250519_145451.jpg"
-        self.modeli = YOLO(r"c:\Users\user\OneDrive\Downloads\amen.pt")
-        self.modelii = YOLO(r"c:\Users\user\OneDrive\Desktop\model_versions\a_6\goodbad-exp3\weights\last.pt")
-        self.modeliii = YOLO(r"c:\Users\user\OneDrive\Downloads\last (1).pt")
--- nms_function.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def apply_nms(boxes, confidences):
-    nms_indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    
-    if len(nms_indices) > 0:
-        nms_indices = nms_indices.flatten()
-    return nms_indices   
